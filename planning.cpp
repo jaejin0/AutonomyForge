0c86fa5490e61399b1ede3845cc976485810442d
jaejin <jaejin0109@gmail.com>
2026-08-13 23:35:35 -0500

    created vertex and edge struct

--- planning.cpp
+++ planning.cpp
@@ -3,18 +3,40 @@
 #include <queue>
 using namespace std;
 
+struct Edge {
+	int target_vertex;
+	int weight;
+};
+
+struct Vertex {
+	int id;
+	vertex<Edge> edges;
+}; 
+
 class GraphPlanner {
 	private:
-		vector<vector<int>> graph;	
+		// variables
+		vector<Vertex> graph;
+
+		// helper functions
+		void set_vertices(vector<vector<int>> edges) {
+			for (vector<vector<int>>::iterator iter = edges.begin(); iter != edges.end(); iter++) {
+					
+			}
+		}
 
 	public:
-		GraphPlanner(vector<vector<int>> new_graph) {
-			graph = new_graph;	
+		GraphPlanner(vector<vector<int>> new_edges, vector<int> new_vertices = {}) {
+			edges = new_edges;
+
+			if (new_vertices == {}) {
+				
+			}
 		}
 
 		vector<int> dijkstra(int source, int target) {
 			priority_queue<int> min_heap;
-		
+
 			for (vector<vector<int>>::iterator iter = graph.begin(); iter != graph.end(); iter++) {
 				cout << (*iter)[0] << endl;		
 			}
