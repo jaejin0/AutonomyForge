ee23a1c8079f7174ba3808d890ffc7a72dab34f8
jaejin <jaejin0109@gmail.com>
2026-08-13 23:50:42 -0500

    initial dijkstra algorithm

--- planning.cpp
+++ planning.cpp
@@ -10,7 +10,7 @@ struct Edge {
 
 struct Vertex {
 	int id;
-	vertex<Edge> edges;
+	vector<Edge> edges;
 }; 
 
 class GraphPlanner {
@@ -18,27 +18,39 @@ class GraphPlanner {
 		// variables
 		vector<Vertex> graph;
 
-		// helper functions
-		void set_vertices(vector<vector<int>> edges) {
-			for (vector<vector<int>>::iterator iter = edges.begin(); iter != edges.end(); iter++) {
-					
-			}
-		}
-
 	public:
-		GraphPlanner(vector<vector<int>> new_edges, vector<int> new_vertices = {}) {
-			edges = new_edges;
-
-			if (new_vertices == {}) {
-				
-			}
+		GraphPlanner(vector<Vertex> new_graph) {
+			graph = new_graph;
 		}
 
-		vector<int> dijkstra(int source, int target) {
+		vector<int> dijkstra(int source_id, int target_id) {
+			bool is_source_valid = false;
+			for (vector<Vertex>::iterator iter = graph.begin(); iter != graph.end(); iter++) {
+				if (iter->id == source_id) {
+					is_source_valid = true;	
+					break;
+				}
+			}
+			if (!is_source_valid) {
+				throw invalid_argument("Source ID does not exist in the current graph.");
+			}
+	
+			bool is_target_valid = false;
+			for (vector<Vertex>::iterator iter = graph.begin(); iter != graph.end(); iter++) {
+				if (iter->id == target_id) {
+					is_target_valid = true;	
+					break;
+				}
+			}
+			if (!is_target_valid) {
+				throw invalid_argument("Target ID does not exist in the current graph.");
+			}
+
+
 			priority_queue<int> min_heap;
 
 			for (vector<vector<int>>::iterator iter = graph.begin(); iter != graph.end(); iter++) {
-				cout << (*iter)[0] << endl;		
+				cout << iter->edges[0] << endl;		
 			}
 			return {};
 		}
@@ -46,9 +58,9 @@ class GraphPlanner {
 
 
 int main() {
-	vector<vector<int>> graph = {{1, 3},
-								 {2, 3}};
-
+	vector<Vertex> graph = {Vertex(1, Edge(2, 10)), Vertex(2, null)};
+	
 	GraphPlanner graph_planner(graph);
+	
 	graph_planner.dijkstra(1, 3);
 };
