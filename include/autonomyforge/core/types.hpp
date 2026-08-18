b7a99b17f33f7841884947313463fcf765c2ff8c
jaejin <jaejin0109@gmail.com>
2026-08-18 01:14:44 -0500

    fixed syntax error

--- include/autonomyforge/core/types.hpp
+++ include/autonomyforge/core/types.hpp
@@ -1,43 +1,44 @@
+#pragma once
 
+#include <cstddef>
 #include <vector>
-
 
 namespace autonomyforge {
 
 using AgentId = std::size_t;
 
 struct Vec2 {
-	double x;
-	double y;
+	double x{0.0};
+	double y{0.0};
 };
 
 
 struct Rect {
-	Vec2 min;
-	Vec2 max;
-}
+	Vec2 min{};
+	Vec2 max{};
+};
 
 struct AgentInfo {
 	AgentId id(0);
-	Vec2 start;
-	Vec2 goal;
-	double radius(0.25);
-	double max_speed(1.0);
+	Vec2 start{};
+	Vec2 goal{};
+	double radius{0.25};
+	double max_speed{1.0};
 };
 
 struct AgentState {
-	AgentId id(0);
-	Vec2 position;
-	Vec2 velocity;
-}
+	AgentId id{0};
+	Vec2 position{};
+	Vec2 velocity{};
+};
 
 struct World {
-	double width(0.0);
-	double height(0.0);
+	double width{0.0};
+	double height{0.0};
 
 	std::vector<Rect> obstacles{};
 	std::vector<AgentInfo> agents{};
-}
+};
 
 
 
