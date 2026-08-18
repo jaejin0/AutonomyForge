b6b6dbe989a8b024b6357d0d2aee5c3cf4b83c41
jaejin <jaejin0109@gmail.com>
2026-08-17 23:44:18 -0500

    created occupancy grid class

--- include/autonomyforge/planning/occupancy_grid.hpp
+++ include/autonomyforge/planning/occupancy_grid.hpp
@@ -4,9 +4,23 @@
 
 
 
-namespace autonomyforge {
+namespace autonomyforge {	
 
-	
+struct GridCell {
+	int x;
+	int y;
+}
+
+
+class OccupancyGrid {
+public:
+	GridCell worldToCell(Vec2);
+	Vec2 cellToWorld(GridCell);
+	bool isInside(GridCell);
+	bool isBlocked(GridCell, double radius);
+}
+
+
 
 
 
