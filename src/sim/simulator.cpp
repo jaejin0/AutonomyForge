69f64a5c2a27516cb8139f526cea935f1f5cce99
jaejin <jaejin0109@gmail.com>
2026-08-19 23:49:00 -0500

    created simulator initialization step

--- src/sim/simulator.cpp
+++ src/sim/simulator.cpp
@@ -1,1 +1,15 @@
 #include "autonomyforge/sim/simulator.hpp"
+
+#include "autonomyforge/core/geometry.hpp"
+
+
+namespace autonomyforge {
+
+	Simulator::Simulator(double dt) : dt_(dt) {
+		if (dt_ <= 0.0) {
+			throw std::invalid_argument("Simulation timestep delta must be positive");
+		}		
+	}
+}
+
+
